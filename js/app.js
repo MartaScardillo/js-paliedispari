08a442e15e92b73f7b4de68a3e2bc9c256b6c50b
Marta Scardillo <martascardillo02@gmail.com>
2023-10-12 15:18:43 +0200

    Add main palindrome thought

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const word = prompt('Inserire una parola')
+
+// function isPalindromo(word) {
+// }
+// A N N A
+// 0 1 2 3 length 4
+i = 0
+word.charAt(i) === word.charAt(word.length - 1 - i)
+console.log(word.charAt(i) === word.charAt(word.length - 1 - i))
