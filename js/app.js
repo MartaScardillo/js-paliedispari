0e4796b7678067c21466905fc4c327cf5794faff
Marta Scardillo <martascardillo02@gmail.com>
2023-10-12 15:38:54 +0200

    Add palindromo not fully completed

--- js/app.js
+++ js/app.js
@@ -4,6 +4,19 @@ const word = prompt('Inserire una parola')
 // }
 // A N N A
 // 0 1 2 3 length 4
-i = 0
-word.charAt(i) === word.charAt(word.length - 1 - i)
-console.log(word.charAt(i) === word.charAt(word.length - 1 - i))
+
+function isPalindromo(word) {
+    for (let i = 0; i < word.length; i++) {
+        const char1 = word.charAt(i);
+        const char2 = word.charAt(word.length - 1 - i)
+        const charCalc = char1 !== char2
+    
+        if (charCalc) {
+            return false
+        }
+    }
+    
+    return true
+}
+
+console.log(isPalindromo(word))
