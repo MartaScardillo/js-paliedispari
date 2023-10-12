d366a44f86ce51b51482ad4fa88481d06a9d5d76
Marta Scardillo <martascardillo02@gmail.com>
2023-10-12 16:41:15 +0200

    ADD while instead of for

--- js/app.js
+++ js/app.js
@@ -1,22 +1,42 @@
-const word = prompt('Inserire una parola')
+const word = prompt('Inserire una parola');
 
-// function isPalindromo(word) {
-// }
 // A N N A
 // 0 1 2 3 length 4
 
+// function isPalindromo(word) {
+
+//     for (let i = 0; i < word.length; i++) {
+//         const char1 = word.charAt(i);
+//         const char2 = word.charAt(word.length - 1 - i)
+//         const charCalc = char1 !== char2
+
+//         if (charCalc) {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
+
 function isPalindromo(word) {
-    for (let i = 0; i < word.length; i++) {
-        const char1 = word.charAt(i);
-        const char2 = word.charAt(word.length - 1 - i)
-        const charCalc = char1 !== char2
-    
+    let firstChar = 0;
+    let lastChar = word.length - 1;
+    let charPali = true;
+
+    while (firstChar < lastChar && charPali) {
+        const char1 = word.charAt(firstChar);
+        const char2 = word.charAt(lastChar);
+        const charCalc = char1 !== char2;
+
         if (charCalc) {
-            return false
+            charPali = false;
         }
+
+        firstChar++
+        lastChar--
     }
-    
-    return true
+
+    return charPali;
 }
 
-console.log(isPalindromo(word))
+console.log(isPalindromo(word));
